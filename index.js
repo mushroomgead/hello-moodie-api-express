abebcc771815616e545a99c9d24d0646cd6cc163
Kate <kate@Kates-MBP.turtleroom>
2021-04-13 16:11:40 +0700

    fix(cors): allow cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const db = require('./db')
-
 
 const app = express()
 const PORT = 4000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
